6a94b48138cd56116f5b82b9f36500cc22042a08
manueljapon <efrain.japon@gmail.com>
2024-01-11 22:26:30 -0500

    Prueba reporte ticket

--- src/main/java/com/mavil/reports/controller/ReportController.java
+++ src/main/java/com/mavil/reports/controller/ReportController.java
@@ -9,34 +9,34 @@ import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.sql.SQLException;
 
 @RestController
-@RequestMapping("/api/reportes")
+@RequestMapping("/tickets")
 public class ReportController {
 
     @Autowired
     JasperReportService jasperReportService;
 
-    @GetMapping("ticket")
-    public ResponseEntity<ByteArrayResource> getItemReport() throws JRException, IOException, SQLException {
+    @GetMapping("get")
+    public ResponseEntity<ByteArrayResource> getTicketByCode(@RequestParam String code) throws JRException, IOException, SQLException {
 
-        byte[] reportContent = jasperReportService.getItemReport();
+        byte[] reportContent = jasperReportService.getItemReport(code);
 
         ByteArrayResource resource = new ByteArrayResource(reportContent);
 
         return ResponseEntity.ok()
-                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .contentType(MediaType.APPLICATION_PDF)
                 .contentLength(resource.contentLength())
                 .header(HttpHeaders.CONTENT_DISPOSITION,
-                        ContentDisposition.attachment()
-                                .filename("item-report.pdf")
+                        ContentDisposition.inline()
+                                .filename("ticket.pdf")
                                 .build().toString())
                 .body(resource);
     }
--- src/main/java/com/mavil/reports/service/JasperReportService.java
+++ src/main/java/com/mavil/reports/service/JasperReportService.java
@@ -1,16 +1,6 @@
 package com.mavil.reports.service;
 
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperExportManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.export.JRPdfExporter;
-import net.sf.jasperreports.export.SimpleExporterInput;
-import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
-import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
-import net.sf.jasperreports.export.SimplePdfReportConfiguration;
+import net.sf.jasperreports.engine.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
@@ -18,7 +8,6 @@ import org.springframework.util.ResourceUtils;
 import javax.sql.DataSource;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,47 +18,15 @@ public class JasperReportService {
     @Autowired
     private DataSource dataSource;
 
-    public byte[] getItemReport() throws FileNotFoundException, JRException, SQLException {
+    public byte[] getItemReport(String ticketId) throws FileNotFoundException, JRException, SQLException {
 
-
-        InputStream employeeReportStream
-                = getClass().getResourceAsStream("/employeeReport.jrxml");
+        File file = ResourceUtils.getFile("/Users/manueljapon/JaspersoftWorkspace/MyReports/ticket2.jrxml");
         JasperReport jasperReport
-                = JasperCompileManager.compileReport(employeeReportStream);
-
-        //File file = getClass().getResourceAsStream("item-report.jrxml");
-//        ResourceUtils.getFile("classpath:item-report.jrxml");
-        //JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+                = JasperCompileManager.compileReport(file.getAbsolutePath());
 
         Map parametros = new HashMap();
-        parametros.put("ptrncod", Integer.valueOf("1"));
-        parametros.put("pesquema", "esquema");
-        parametros.put("pathfondo", "pathFondo");
+        parametros.put("ticketid", Integer.valueOf(ticketId));
         JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parametros, dataSource.getConnection());
-
-        /*JRPdfExporter exporter = new JRPdfExporter();
-
-        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-        exporter.setExporterOutput(
-                new SimpleOutputStreamExporterOutput("employeeReport.pdf"));
-
-        SimplePdfReportConfiguration reportConfig
-                = new SimplePdfReportConfiguration();
-        reportConfig.setSizePageToContent(true);
-        reportConfig.setForceLineBreakPolicy(false);
-
-        SimplePdfExporterConfiguration exportConfig
-                = new SimplePdfExporterConfiguration();
-        exportConfig.setMetadataAuthor("baeldung");
-        exportConfig.setEncrypted(true);
-        exportConfig.setAllowedPermissionsHint("PRINTING");
-
-        exporter.setConfiguration(reportConfig);
-        exporter.setConfiguration(exportConfig);
-
-        exporter.exportReport();
-
-         */
 
         byte[] reportContent = JasperExportManager.exportReportToPdf(jasperPrint);
 
