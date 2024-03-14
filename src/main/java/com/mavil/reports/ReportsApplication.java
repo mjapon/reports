b5d4f87198fbb594cb8167ff0a510548acbcc575
manueljapon <efrain.japon@gmail.com>
2024-03-14 16:38:11 -0500

    se adecúa para tomcat

--- src/main/java/com/mavil/reports/ReportsApplication.java
+++ src/main/java/com/mavil/reports/ReportsApplication.java
@@ -2,11 +2,12 @@ package com.mavil.reports;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-public class ReportsApplication {
+public class ReportsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReportsApplication.class, args);
