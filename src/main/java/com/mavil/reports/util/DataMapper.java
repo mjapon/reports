365e79025d3e84e7bd7d317d139604f933724819
manueljapon <efrain.japon@gmail.com>
2024-03-25 21:02:08 -0500

    version reportes en produ

---
+++ src/main/java/com/mavil/reports/util/DataMapper.java
@@ -0,0 +1,28 @@
+package com.mavil.reports.util;
+
+import com.mavil.reports.vo.GenBalanceRequestVo;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Service
+public class DataMapper {
+
+    public Context setData(GenBalanceRequestVo request) {
+
+        Context context = new Context();
+
+        Map<String, Object> data = new HashMap<>();
+
+        data.put("items", request.getItems());
+        data.put("titulo", request.getTitulo());
+        data.put("resumen", request.getResumen());
+
+        context.setVariables(data);
+
+        return context;
+    }
+}
